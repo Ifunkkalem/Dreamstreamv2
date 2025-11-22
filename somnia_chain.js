2be0a2fc53bd8a496be77c74437cfa926fd9e434
Ifunkkalem <hilldanime@gmail.com>
2025-11-22 20:52:18 +0700

    Update somnia_chain.js

--- somnia_chain.js
+++ somnia_chain.js
@@ -1,21 +1,40 @@
-// somnia_chain.js
 async function addOrSwitchSomnia() {
-  if (!window.ethereum) throw new Error('MetaMask not found');
+  if (!window.ethereum)
+    throw new Error('MetaMask not found');
+
   const chain = {
-    chainId: window.SOMNIA_CONFIG.chainIdHex,
-    chainName: 'Somnia Testnet',
-    rpcUrls: [window.SOMNIA_CONFIG.rpc],
-    nativeCurrency: { name: 'Somnia Token', symbol: 'STT', decimals: 18 },
-    blockExplorerUrls: ['https://explorer-testnet.somnia.network']
+    chainId: "0xc488", // HEX dari 50312
+    chainName: "Somnia Testnet",
+    rpcUrls: ["https://dream-rpc.somnia.network"],
+    nativeCurrency: {
+      name: "Somnia Test Token",
+      symbol: "STT",
+      decimals: 18
+    },
+    blockExplorerUrls: ["https://explorer-testnet.somnia.network"]
   };
+
   try {
-    await window.ethereum.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: chain.chainId }] });
+    // Switch chain kalau sudah ada
+    await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: chain.chainId }]
+    });
     return { switched: true };
+
   } catch (err) {
-    if (err.code === 4902 || (err.message && err.message.includes('Unrecognized chain'))) {
-      await window.ethereum.request({ method: 'wallet_addEthereumChain', params: [chain] });
+
+    // Chain belum ditambahkan
+    if (err.code === 4902 || err.message.includes("Unrecognized")) {
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [chain]
+      });
       return { added: true };
-    } else throw err;
+    }
+
+    throw err;
   }
 }
+
 window.addOrSwitchSomnia = addOrSwitchSomnia;
